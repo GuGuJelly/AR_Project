88896aee672bdac0dfe9a7cebacabff2d99db82e
GuGuJelly <jellypuding@naver.com>
2024-10-09 23:50:28 +0900

    [FEAT] 슬라임의 공격모션 구현
    화면 터치하면 공격 모션이 나온다

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -1,31 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
 
 public class Attack : MonoBehaviour
 {
-    [SerializeField] GameObject target;
-    [SerializeField] CapsuleCollider myCollider;
-    private Touch touch;
-    
-
-    private void Awake()
-    {
-        myCollider = GetComponent<CapsuleCollider>();
-    }
+    [SerializeField] Animator animator;
 
     private void Update()
     {
-        
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                StartCoroutine(ChangedState());
+            }
+        }else if (AttackIdleStateChange != null)
+        {
+            StopCoroutine(AttackIdleStateChange);
+            AttackIdleStateChange = null;
+        }
     }
-    private void CheckTarget()
+
+    Coroutine AttackIdleStateChange;
+
+    public IEnumerator ChangedState()
     {
-        
-    }
-
-    private void Attack()
-    {
-
-    }
-
+        yield return new WaitForSeconds(1f);
+        animator.SetBool("Action", true);
+        yield return new WaitForSeconds(2f);
+        animator.SetBool("Action", false);
+        yield return new WaitForSeconds(1f);
+    } 
 }
